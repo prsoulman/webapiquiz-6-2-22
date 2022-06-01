8839d6db94f33c230ab2ab45f7c437941710a452
prsoulman <60936175+prsoulman@users.noreply.github.com>
2022-06-01 11:01:04 -0500

    added to the psuedo code a general outline, added html buttons, and CSS body and question cards

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+//pseudocode
+//create variables at the top
+var score = 
+ 
+//create a score function
+
+//highscore page
+//todo: create a localstorage for the score element
+//todo: create a highscore function
+//todo: store the rank in local storage
+
+//creating the questions array
+//question 1
+//question 2
+//question 3
+//question 4
+
+
+
+//timer function
+
+//Start Questions
+
+//reset
+
+//Test Loop attached to timer 
+
+//Next Question
