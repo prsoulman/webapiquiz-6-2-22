f0e0ee124c8242b336ca87b625c4f8da1bf4f14e
prsoulman <60936175+prsoulman@users.noreply.github.com>
2022-06-01 11:05:39 -0500

    created variables in javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,16 @@
 //pseudocode
 //create variables at the top
-var score = 
+var win = document.querySelector(".win");
+var lose = document.querySelector(".lose");
+var timerElement = document.querySelector(".timer-count");
+var startButton = document.querySelector(".start-button");
+
+var numBlanks = 0;
+var winCounter = 0;
+var loseCounter = 0;
+var isWin = false;
+var timer;
+var timerCount;
  
 //create a score function
 
