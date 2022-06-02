0c4c0c15b51936ec1e3a2404301bd72bccaad8de
David Rodriguez <davidrodriguezcomedy@gmail.com>
2022-06-02 11:32:06 -0500

    wrote a nonfunctioning loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,8 +12,7 @@ var loseCounter = 0;
 var isWin = false;
 var timer;
 var timerCount;
- 
-//create a score function
+
 
 //highscore page
 //todo: create a localstorage for the score element 
@@ -25,34 +24,37 @@ var timerCount;
 //question 1
 var Questions = [
 {
-    text:'question 1',
-    choices:['a','b','c','d'],
+    text:'With what number does an index start?',
+    choices:['a-0','b-1','c-3','d-4'],
     answer:'a'
 },
+//question 2
 {
     text:'question 2',
     choices:['a','b','c','d'],
     answer:'c'
 },
+//question 3
 {
     text:'question 3',
     choices:['a','b','c','d'],
     answer:'b'
 },
+//question 4
 {
     text:'question 4',
     choices:['a','b','c','d'],
     answer:'d'
 },
+//question 5
 {
     text:'question 5',
     choices:['a','b','c','d'],
     answer:'b'
 },
 ]
-//question 2
-//question 3
-//question 4
+
+
 
 
 function webquiz () {
@@ -70,14 +72,40 @@ function webquiz () {
         console.log(this.value);
     }
     answerDiv.appendChild(button);
-    
-    })
 
+    })
+    if (choices === answer) {
+
+    }
+
+}
+
+//I need a function to loop through the webquiz function
+function looper () {
+    var loopEl = "";
+    for (q; q < [0]; i++) {
+        var index = Math.floor(Math.random()*Questions.length);
+        var char = Questions[index];
+        Questions += char;
+    }
+ 
+   return randomPass;
+   }
+
+
+//create a score function to display score attached to code local storage function
+
+
+//function stores the score for the high score page
+function localScore () {
+    
+    setItem.JSON.stringify(webquiz());
+    getItem.JSON.parse(webquiz());
 }
 
 //timer function
 function countdown() {
-    var timer = 76;
+    var timer = 30;
     var timeCount;
     /**this is the timer funtion which will start counting as soon as the quiz starts*/
     function setupTimer() {
@@ -105,6 +133,8 @@ function countdown() {
 //Next Question
 
 //pull information from localstorage for high score
+
+//This is the start button that hides the questions until clicked
 startButton.addEventListener('click', function(){
     document.querySelector('.start-div').classList.add('hide');
     document.querySelector('.question-card').classList.remove('hide');
